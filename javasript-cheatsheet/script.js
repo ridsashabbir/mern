4cf68b9dafdb616f95bafb7f2fa88e94c0032039
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-07-25 12:11:40 +0500

    variable declaration

--- javasript-cheatsheet/script.js
+++ javasript-cheatsheet/script.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+let a = 67;
+console.log(a);
+a = "Ridsa"; // allowed, as we used let
+console.log(a);
+
+var b = 11;
+console.log(b);
+b = "Ridsa"; // also allowed but scope is different from let
+console.log(b);
+
+const c = 23;
+console.log(c);
+c = "Ridsa"; // not allowed, const would not let you change the value of variable.
+console.log(c);
