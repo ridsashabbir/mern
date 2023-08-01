db0e535f76d43098c33837750677cde1a7e993af
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-01 18:01:48 +0500

    variables in js

---
+++ react-js-cheatsheet/JS - Refresher/script.js
@@ -0,0 +1,12 @@
+// variables in javascript
+let a = 34;
+a = 4;
+console.log(a);
+
+const b = 34;
+// b = 4; // you cannot change value of const
+console.log(b);
+
+var c = 34;
+var c = 4; // var don't give you error even if variable id redeclared
+console.log(c);
