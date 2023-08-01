78f482b822d288166ce75984047ff08b26921fd9
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-01 19:24:32 +0500

    mix javascript within jsx

--- react-js-cheatsheet/textutils/src/App.js
+++ react-js-cheatsheet/textutils/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
 
+let name = "Ridsa";
 function App() {
   return (
     <>
-      <div className="blank">Hello World</div>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+      </nav>
+
+      <div className="container">
+        {/* everthing within braces will work as javascript, below is an example */}
+        <h1>Hello {name}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          error placeat voluptatibus repellendus obcaecati odio nesciunt eaque
+          ipsam cupiditate odit blanditiis aliquid libero minima, nostrum
+          voluptas doloremque adipisci? A minus sit fugit maiores vel!
+        </p>
+      </div>
     </>
   );
 }
