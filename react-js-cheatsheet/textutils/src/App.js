00059b11cecd95c14dd82e9a82e289a638109564
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-02 14:17:58 +0500

    starter

--- react-js-cheatsheet/textutils/src/App.js
+++ react-js-cheatsheet/textutils/src/App.js
@@ -1,28 +1,7 @@
 import "./App.css";
 
-let name = "Ridsa";
 function App() {
-  return (
-    <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Services</li>
-      </nav>
-
-      <div className="container">
-        {/* everthing within braces will work as javascript, below is an example */}
-        <h1>Hello {name}</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-          error placeat voluptatibus repellendus obcaecati odio nesciunt eaque
-          ipsam cupiditate odit blanditiis aliquid libero minima, nostrum
-          voluptas doloremque adipisci? A minus sit fugit maiores vel!
-        </p>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
