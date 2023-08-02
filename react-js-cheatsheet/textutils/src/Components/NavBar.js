0587ac3afe65fec4a63197db1728c9388b2bf5ab
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-02 15:20:29 +0500

    set propTypes

--- react-js-cheatsheet/textutils/src/Components/NavBar.js
+++ react-js-cheatsheet/textutils/src/Components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function NavBar(props) {
   return (
@@ -47,3 +48,8 @@ export default function NavBar(props) {
     </nav>
   );
 }
+
+NavBar.propTypes = {
+  title: PropTypes.string,
+  about: PropTypes.string,
+};
