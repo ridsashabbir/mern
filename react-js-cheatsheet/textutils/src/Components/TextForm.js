76a2615bf6319d6568b7f2f4e063ec869801364a
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-04 22:58:52 +0500

    set state and handle event

--- react-js-cheatsheet/textutils/src/Components/TextForm.js
+++ react-js-cheatsheet/textutils/src/Components/TextForm.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter your text here");
+
+  const handleUpClick = () => {
+    console.log("upper case was clicked");
+    setText("you clicked it stupid");
+  };
+
+  const handleOnChange = (event) => {
+    console.log("on change");
+    setText(event.target.value);
+  };
+
   return (
-    <div class="mb-3">
-      <h1>{props.heading}</h1>
-      <textarea className="form-control" id="myBox" rows="10"></textarea>
+    <div>
+      <div className="mb-3">
+        <h1>{props.heading}</h1>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="10"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
     </div>
   );
 }
