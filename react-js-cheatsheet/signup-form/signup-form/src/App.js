7b202c8451c87ffbac06f5bfe5093e35c74cd76c
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-05 23:15:29 +0500

    signup form

---
+++ react-js-cheatsheet/signup-form/signup-form/src/App.js
@@ -0,0 +1,7 @@
+import Signup from "./Components/Signup";
+
+function App() {
+  return <Signup></Signup>;
+}
+
+export default App;
---
+++ react-js-cheatsheet/signup-form/signup-form/src/Components/Signup.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Signup() {
+  return (
+    <div>
+      <div className="container">
+        <h1>Signup Form</h1>
+        <form>
+          <div className="mb-3">
+            <label className="from-label">Full Name</label>
+            <input type="text" name="fullName" />
+          </div>
+          <div className="mb-3">
+            <label className="from-label">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className="mb-3">
+            <label className="from-label">Password</label>
+            <input type="password" name="password" />
+          </div>
+          <div className="mb-3">
+            <label className="from-label">Confirm Password</label>
+            <input type="password" name="confirmPassword" />
+          </div>
+          <div className="mb-3">
+            <label className="from-label">Age</label>
+            <input type="text" name="age" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
