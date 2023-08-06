5f0d71dd72214beb8db43874865e6416215dcd5c
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-06 18:57:23 +0500

    basic css

--- react-js-cheatsheet/signup-form/signup-form/src/Components/Signup.js
+++ react-js-cheatsheet/signup-form/signup-form/src/Components/Signup.js
@@ -7,24 +7,39 @@ export default function Signup() {
         <h1>Signup Form</h1>
         <form>
           <div className="mb-3">
-            <label className="form-label">Full Name</label>
-            <input type="text" name="fullName" />
+            {/* <label className="form-label">Full Name</label> */}
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              required
+            />
           </div>
           <div className="mb-3">
-            <label className="from-label">Email</label>
-            <input type="email" name="email" />
+            {/* <label className="form-label">Email</label> */}
+            <input type="email" name="email" placeholder="Email" required />
           </div>
           <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" name="password" />
+            {/* <label className="form-label">Password</label> */}
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
           </div>
           <div className="mb-3">
-            <label className="form-label">Confirm Password</label>
-            <input type="password" name="confirmPassword" />
+            {/* <label className="form-label">Confirm Password</label> */}
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+            />
           </div>
           <div className="mb-3">
-            <label className="form-label">Age</label>
-            <input type="text" name="age" />
+            {/* <label className="form-label">Age</label> */}
+            <input type="text" name="age" placeholder="Age" required />
           </div>
           <button type="submit" className="btn btn-primary">
             Submit
