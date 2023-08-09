96c479f32c27355422fff9b0a04aef6205a5322a
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-09 22:23:44 +0500

    added about button

--- react-js-cheatsheet/textutils/src/App.js
+++ react-js-cheatsheet/textutils/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import TextForm from "./Components/TextForm";
+// import TextForm from "./Components/TextForm";
+import About from "./Components/About";
 
 function App() {
   return (
     <>
       <NavBar title="TextUtils" about="About TextUtils" />
-      <div className="container my-5">
+      {/* <div className="container my-5">
         <TextForm heading="Enter your Text to Analyze" />
-      </div>
+      </div> */}
+      <About />
 
       {/* <NavBar /> */}
     </>
