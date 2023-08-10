44d0bc66e724cc9ad3b93d9fba777fda698797c6
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-10 10:37:36 +0500

    enhancing dark mode

--- react-js-cheatsheet/textutils/src/App.js
+++ react-js-cheatsheet/textutils/src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 import NavBar from "./Components/NavBar";
 import TextForm from "./Components/TextForm";
 // import About from "./Components/About";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
   return (
     <>
-      <NavBar title="TextUtils" about="About TextUtils" />
+      <NavBar
+        title="TextUtils"
+        about="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="container my-5">
         <TextForm heading="Enter your Text to Analyze" />
       </div>
--- react-js-cheatsheet/textutils/src/Components/NavBar.js
+++ react-js-cheatsheet/textutils/src/Components/NavBar.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 
 export default function NavBar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -43,6 +45,26 @@ export default function NavBar(props) {
               Search
             </button>
           </form>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            >
+              Enable DarkMode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
