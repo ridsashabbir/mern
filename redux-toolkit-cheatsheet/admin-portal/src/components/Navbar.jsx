bca10822138b8b26c0be11e62e0c8584ad5aa2fa
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-10 20:51:39 +0500

    added heading

---
+++ redux-toolkit-cheatsheet/admin-portal/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+    const myStyle={
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+        width: '100%',
+        
+        fontSize: '40px'
+      }
+    return (
+        <div className="container ">
+            <h2 style={myStyle}>Admin Portal</h2>
+        </div>
+    );
+  };
+  
+  export default App;
+  
