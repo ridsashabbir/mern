3522736c775d539b25b24dd8ee64cf559a3eb8de
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-10 10:40:44 +0500

    change color of baby to dark

--- react-js-cheatsheet/textutils/src/App.js
+++ react-js-cheatsheet/textutils/src/App.js
@@ -9,8 +9,10 @@ function App() {
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
+      document.body.style.backgroundColor = "black";
     } else {
       setMode("light");
+      document.body.style.backgroundColor = "white";
     }
   };
   return (
