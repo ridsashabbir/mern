90d860e214f300644f16aa904448c7c8cd5f3793
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-11 22:39:52 +0500

    ..

--- redux-toolkit-cheatsheet/admin-portal/src/App.jsx
+++ redux-toolkit-cheatsheet/admin-portal/src/App.jsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 import Navbar from './components/Navbar'
+// import UserDetails from './components/UserDetails'
 
 function App() {
   return (
+    <>
     <Navbar/>
+    {/* <UserDetails/> */}
+    </>
   )
 }
 
