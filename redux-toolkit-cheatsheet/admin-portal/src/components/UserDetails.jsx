959feefd61f5d4c89296288ed3123647344dc65e
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-12 22:23:07 +0500

    ..

--- redux-toolkit-cheatsheet/admin-portal/src/components/UserDetails.jsx
+++ redux-toolkit-cheatsheet/admin-portal/src/components/UserDetails.jsx
@@ -2,16 +2,32 @@
 import DeleteAllUsers from './DeleteAllUsers'
 
 function UserDetails() {
+    const myStyle = {
+        marginLeft: '20%',
+        fontSize: '2.0rem',
+        fontWeight: 'bold'
+    }
+    const myStyleB = {
+        marginLeft: '40%',
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        padding: '10px 10px',
+        borderRadius: '15px',
+        backgroundColor: '#241468',
+        color: 'white',
+        // marginTop: '10px'
+    }
   return (
     <>
     <div className="content">
         <div className="admin-portal">
-            <div className="admin-subtitle">
+            <div className="admin-subtitle" style={myStyle}>
                 List of User Details
+            
+            <button className='btn add-btn' style={myStyleB}>Add New User</button>
             </div>
-            <button>Add New User</button>
         </div>
-        <ul>
+        <ul >
             <li>Hi</li>
             <li>Hii</li>
         </ul>
