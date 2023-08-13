5da33e4cf881b361c1c4fc4a616fd3399c2bad32
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-13 12:53:12 +0500

    Display User Data using useSelector() & Action Creators

--- redux-toolkit-cheatsheet/admin-portal/src/components/DisplayUsers.jsx
+++ redux-toolkit-cheatsheet/admin-portal/src/components/DisplayUsers.jsx
@@ -1,11 +1,17 @@
 import { useSelector } from "react-redux";
 
 const DisplayUsers = () => {
-    const data = useSelector((state) => {
-        return state.users;
-    })
-    console.log(data)
-return <></>
-}
+  const data = useSelector((state) => {
+    return state.users;
+  });
+  console.log(data);
+  return (
+    <>
+      {data.map((user, id) => {
+        return <li key={id}>{user}</li>;
+      })}
+    </>
+  );
+};
 // const Wrapper = styled.section;
 export default DisplayUsers;
--- redux-toolkit-cheatsheet/admin-portal/src/components/UserDetails.jsx
+++ redux-toolkit-cheatsheet/admin-portal/src/components/UserDetails.jsx
@@ -1,52 +1,58 @@
 // import React from 'react'
-import DeleteAllUsers from './DeleteAllUsers'
-import { fakeUserData } from '../api'
-import { useDispatch } from 'react-redux'
-import { addUser } from '../store/UserSlice'
-import DisplayUsers from '../components/DisplayUsers'
+import DeleteAllUsers from "./DeleteAllUsers";
+import { fakeUserData } from "../api";
+import { useDispatch } from "react-redux";
+import { addUser } from "../store/UserSlice";
+import DisplayUsers from "../components/DisplayUsers";
 
 const UserDetails = () => {
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch(); 
-
-    const addNewUser = (payload) => {
-        console.log(payload)
-        dispatch(addUser(payload))
-    }
-    const myStyle = {
-        marginLeft: '20%',
-        fontSize: '2.0rem',
-        fontWeight: 'bold'
-    }
-    const myStyleB = {
-        marginLeft: '40%',
-        fontSize: '1.5rem',
-        fontWeight: 'bold',
-        padding: '10px 10px',
-        borderRadius: '15px',
-        backgroundColor: '#241468',
-        color: 'white',
-        // marginTop: '10px'
-    }
+  const addNewUser = (payload) => {
+    console.log(payload);
+    dispatch(addUser(payload));
+  };
+  const myStyle = {
+    marginLeft: "20%",
+    fontSize: "2.0rem",
+    fontWeight: "bold",
+  };
+  const myStyleB = {
+    marginLeft: "40%",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    padding: "10px 10px",
+    borderRadius: "15px",
+    backgroundColor: "#241468",
+    color: "white",
+    // marginTop: '10px'
+  };
+  const myStyleU = {
+    marginLeft: "20%",
+  };
   return (
-    
     <>
-    <div className="content">
+      <div className="content">
         <div className="admin-portal">
-            <div className="admin-subtitle" style={myStyle}>
-                List of User Details
-            
-            <button className='btn add-btn' style={myStyleB} onClick={() => addNewUser(fakeUserData())}>Add New User</button>
-            </div>
+          <div className="admin-subtitle" style={myStyle}>
+            List of User Details
+            <button
+              className="btn add-btn"
+              style={myStyleB}
+              onClick={() => addNewUser(fakeUserData())}
+            >
+              Add New User
+            </button>
+          </div>
         </div>
-        <ul style={myStyle}>
-            <DisplayUsers/>
+        <ul style={myStyleU}>
+          <DisplayUsers />
         </ul>
         <hr />
-        <DeleteAllUsers/>
-    </div>
+        <DeleteAllUsers />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
