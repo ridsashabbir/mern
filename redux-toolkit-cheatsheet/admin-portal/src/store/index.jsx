afae16a4e050aed0ca93a27e6f1fbd173a485471
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-13 11:59:53 +0500

    Update index.jsx

--- redux-toolkit-cheatsheet/admin-portal/src/store/index.jsx
+++ redux-toolkit-cheatsheet/admin-portal/src/store/index.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./UserSlice";
+import  userSlice  from "./UserSlice";
 
 const store = configureStore({
     reducer: {
