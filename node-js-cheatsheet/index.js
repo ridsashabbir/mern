e8e417c8e05c99c932865d5ba3595ef4237349fe
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-18 19:46:53 +0500

    append data of file

--- node-js-cheatsheet/index.js
+++ node-js-cheatsheet/index.js
@@ -4,3 +4,10 @@
 // 3- multiline code
 // 4- use (_) to get the last result
 // 5- we can use editor mode
+
+const fs = require("fs");
+
+// creating a new file
+fs.writeFileSync("read.txt", "i am ridsa shabbir");
+fs.writeFileSync("read.txt", "hello world, i am ridsa shabbir ");
+fs.appendFileSync("read.txt", "i am appending this data");
