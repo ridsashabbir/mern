957d3836b6a21cbf811a1db5041fade6c16c1b47
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-19 14:06:48 +0500

    append data in async

--- node-js-cheatsheet/fsAsync/index.js
+++ node-js-cheatsheet/fsAsync/index.js
@@ -3,4 +3,11 @@ const fs = require("fs");
 // create a file in async
 fs.writeFile("fsAsync/read.txt", "i am in the file ", (err) => {
   console.log("file is created");
+  console.log(err);
+});
+
+// append data after existing data in file
+fs.appendFile("fsAsync/read.txt", " I am appended data", (err) => {
+  console.log("data is appended");
+  console.log(err);
 });
