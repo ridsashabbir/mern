e08cfaf0d93567f082bcc7250b0f280b9c57df83
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-19 14:42:51 +0500

    crud in async

---
+++ node-js-cheatsheet/challenge2/crud.js
@@ -0,0 +1,10 @@
+// Challenge # 1 :
+
+// - create a new folder
+// - create file in it named bio.txt and add data to it
+// - add more data in the file at the end of existing data
+// - read the file without getting buffer data
+// - rename file to mybio.txt
+// - now delete both file and folder
+
+// CRUD : Create - Update - Read - Delete
