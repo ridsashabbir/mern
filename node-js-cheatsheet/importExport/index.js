1e60d17c741fca645c36cdb5aab036d95ba2e600
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-20 15:41:21 +0500

    import and export a module in node js

---
+++ node-js-cheatsheet/importExport/index.js
@@ -0,0 +1,5 @@
+// calling the function here which was made in operator.js
+// we are importing modules in this file
+const add = require("./operator");
+
+console.log(add(4, 6));
---
+++ node-js-cheatsheet/importExport/operator.js
@@ -0,0 +1,7 @@
+// define a function here and call it in index.js
+//we are exporting modules from this filev
+const add = (a, b) => {
+  return a + b;
+};
+
+module.exports = add;
