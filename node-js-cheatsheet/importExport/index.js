9beb561b2e801c64cac0dd060a98f1949c58061c
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-20 15:49:53 +0500

    access methods through object destructuring

--- node-js-cheatsheet/importExport/index.js
+++ node-js-cheatsheet/importExport/index.js
@@ -1,7 +1,20 @@
 // calling the function here which was made in operator.js
 // we are importing modules in this file
+
+// 1- access methos using dot
 const oper = require("./operator");
 
 console.log(oper.add(14, 6));
 console.log(oper.sub(14, 6));
-// console.log(add(4, 6));
+
+// 2- access methods through object destructuring
+const { add, sub } = require("./operator");
+
+console.log(add(14, 6));
+console.log(sub(14, 6));
+
+// 3- you can change the order of passing arugtments too
+const { sub, add } = require("./operator");
+
+console.log(sub(4, 6));
+console.log(add(14, 6));
