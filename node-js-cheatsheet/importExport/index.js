ef69d3967932d7c84b65743c66d4623384f34c60
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-20 15:44:05 +0500

    import and export multiple modules

--- node-js-cheatsheet/importExport/index.js
+++ node-js-cheatsheet/importExport/index.js
@@ -1,5 +1,7 @@
 // calling the function here which was made in operator.js
 // we are importing modules in this file
-const add = require("./operator");
+const oper = require("./operator");
 
-console.log(add(4, 6));
+console.log(oper.add(14, 6));
+console.log(oper.sub(14, 6));
+// console.log(add(4, 6));
--- node-js-cheatsheet/importExport/operator.js
+++ node-js-cheatsheet/importExport/operator.js
@@ -4,4 +4,9 @@ const add = (a, b) => {
   return a + b;
 };
 
-module.exports = add;
+const sub = (a, b) => {
+  return a - b;
+};
+
+module.exports.add = add;
+module.exports.sub = sub;
