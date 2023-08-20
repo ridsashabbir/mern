7619138e05c4b35c4e75e0d99013d1117e2969ab
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-20 15:52:21 +0500

    passing another module

--- node-js-cheatsheet/importExport/index.js
+++ node-js-cheatsheet/importExport/index.js
@@ -2,19 +2,20 @@
 // we are importing modules in this file
 
 // 1- access methos using dot
-const oper = require("./operator");
+// const oper = require("./operator");
 
-console.log(oper.add(14, 6));
-console.log(oper.sub(14, 6));
+// console.log(oper.add(14, 6));
+// console.log(oper.sub(14, 6));
 
 // 2- access methods through object destructuring
-const { add, sub } = require("./operator");
+const { add, sub, mul } = require("./operator");
 
 console.log(add(14, 6));
 console.log(sub(14, 6));
+console.log(mul(10, 6));
 
 // 3- you can change the order of passing arugtments too
-const { sub, add } = require("./operator");
+// const { sub, add } = require("./operator");
 
-console.log(sub(4, 6));
-console.log(add(14, 6));
+// console.log(sub(4, 6));
+// console.log(add(14, 6));
--- node-js-cheatsheet/importExport/operator.js
+++ node-js-cheatsheet/importExport/operator.js
@@ -8,5 +8,10 @@ const sub = (a, b) => {
   return a - b;
 };
 
+const mul = (a, b) => {
+  return a * b;
+};
+
 module.exports.add = add;
 module.exports.sub = sub;
+module.exports.mul = mul;
