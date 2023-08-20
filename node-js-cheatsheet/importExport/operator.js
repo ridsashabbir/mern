3a6572e55545b860317f4887342be8b21ae83d1d
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-20 15:53:24 +0500

    export all modules in one object

--- node-js-cheatsheet/importExport/operator.js
+++ node-js-cheatsheet/importExport/operator.js
@@ -12,6 +12,8 @@ const mul = (a, b) => {
   return a * b;
 };
 
-module.exports.add = add;
-module.exports.sub = sub;
-module.exports.mul = mul;
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.mul = mul;
+
+module.exports = { add, sub, mul };
