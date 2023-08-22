f7407bcd3669cb020714973d4146c117118ca704
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-22 12:54:25 +0500

    create web server in node js

---
+++ node-js-cheatsheet/httpWebServer/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("Hello from the other side");
+});
+
+// first argument would be the port number and second argument would be the local host server
+// port number should be the one that is unused at the moment, here we are using 8000
+// third argument would be the call back function
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to the port number 8000");
+});
+
+// type localhost:8000 on your browser to view your server that you just created
