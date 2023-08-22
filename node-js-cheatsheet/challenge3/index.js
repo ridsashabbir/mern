6c577ae4a3136bbb9bc7cc0b4b72d1d09e9e3823
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-22 13:54:08 +0500

    add json data into another file

--- node-js-cheatsheet/challenge3/index.js
+++ node-js-cheatsheet/challenge3/index.js
@@ -1,8 +1,10 @@
 // 1- convert object to JSON
-// 2- add data into another file
+// 2- add json data into another file
 // 3- read file
 // 4- convert json to object'
 // 5- console.log
+
+const fs = require("fs");
 
 // 1- convert object to JSON
 const bioData = {
@@ -12,3 +14,9 @@ const bioData = {
 };
 const jsonData = JSON.stringify(bioData);
 console.log(jsonData);
+
+// 2- add json data into another file
+fs.writeFile("json1.json", jsonData, (err) => {
+  console.log("data added");
+  console.log(err);
+});
