209254ea7d3806395b12657a23cdd0082607110f
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-22 15:16:02 +0500

    create api

--- node-js-cheatsheet/httpWebServer/index.js
+++ node-js-cheatsheet/httpWebServer/index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
   //   console.log(req.url);
   if (req.url == "/") {
@@ -7,6 +9,15 @@ const server = http.createServer((req, res) => {
     res.end("Hello from about");
   } else if (req.url == "/contact") {
     res.end("Hello from contact");
+  } else if (req.url == "/userapi") {
+    fs.readFile(`${__dirname}/userAPI/userapi.json`, (err, data) => {
+      console.log(err);
+      console.log(data.toString());
+      const objData = JSON.parse(data.toString());
+      console.log(objData);
+      // res.end(data.toString());
+      res.end(objData[0].name);
+    });
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("<h1> 404 error. Page does not exist </h1>");
