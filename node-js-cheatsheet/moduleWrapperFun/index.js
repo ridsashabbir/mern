c01568e4ee3cfd5168a314cfdbd320d35abd8337
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-22 12:27:51 +0500

    Module Wrapper Function

---
+++ node-js-cheatsheet/moduleWrapperFun/index.js
@@ -0,0 +1,10 @@
+(function () {
+  const name = "ridsa";
+  console.log(name);
+})();
+
+// you cannot access it outside the module wrapper function
+// console.log(name);
+
+console.log(__dirname);
+console.log(__filename);
