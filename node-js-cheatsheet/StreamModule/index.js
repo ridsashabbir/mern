c7bd773ddc3cf3905ae57f23d2d965cf8f172537
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-24 22:02:20 +0500

    stream in node js

--- node-js-cheatsheet/StreamModule/index.js
+++ node-js-cheatsheet/StreamModule/index.js
@@ -7,14 +7,31 @@ const fs = require("fs");
 const http = require("http");
 
 // first way
+// const server = http.createServer();
+// server.on("request", (req, res) => {
+//   fs.readFile("input.txt", (err, data) => {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     res.end(data.toString());
+//   });
+// });
+
+// server.listen(8000, "127.0.0.1");
+
+// second way
 const server = http.createServer();
 server.on("request", (req, res) => {
-  fs.readFile("input.txt", (err, data) => {
-    if (err) {
-      return console.error(err);
-    }
-    res.end(data.toString());
+  const rstream = fs.createReadStream("input.txt");
+  rstream.on("data", (chunkdata) => {
+    res.write(chunkdata);
+  });
+  rstream.on("end", () => {
+    res.end();
+  });
+  rstream.on("error", (err) => {
+    console.log(err);
+    res.end(" error occured");
   });
 });
-
 server.listen(8000, "127.0.0.1");
