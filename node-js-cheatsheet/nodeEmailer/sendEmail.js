913be71b5b809284c5ae038e873fe09e4a72084e
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-24 23:43:32 +0500

    send mail with defined transport object

--- node-js-cheatsheet/nodeEmailer/sendEmail.js
+++ node-js-cheatsheet/nodeEmailer/sendEmail.js
@@ -13,6 +13,17 @@ const sendEmail = async (req, res) => {
     },
   });
 
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Ridsa Shabbir 👻" <reese.schumm@ethereal.email>', // sender address
+    to: "ridsashabbir123@gmail.com", // list of receivers
+    subject: "Hello Ridsa", // Subject line
+    text: "Hello Ridsa, how are you?", // plain text body
+    html: "<b>first email sent</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
   res.send("i am sending email");
 };
 
